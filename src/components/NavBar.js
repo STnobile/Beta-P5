ab3b3132c18f26c38446268f19da4023f15c7b28
STnobile <stnobile0@gmail.com>
2023-09-09 11:36:27 +0000

    added the drop down menu handles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,12 +6,16 @@ import { NavLink } from 'react-router-dom';
 import { useCurrentUser, useSetCurrentUser} from '../contexts/CurrentUserContext';
 import Avatar from './Avatar';
 import axios from 'axios';
+import useClickOutsideToggle from '../hooks/useClickOutsideToggle';
 
 
 
 const NavBar = () => {
     const currentUser = useCurrentUser();
     const setCurrentUser = useSetCurrentUser();
+
+
+    const {expanded, setExpanded, ref} = useClickOutsideToggle();
 
     const handleSignOut = async () => {
         try {
@@ -90,7 +94,7 @@ const NavBar = () => {
 
 
     return (
-        <Navbar className={styles.NavBar} expand="md" fixed="top">
+        <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top">
             <Container>
                 <NavLink to="/">
                     <Navbar.Brand href="#home">
@@ -98,7 +102,11 @@ const NavBar = () => {
                     </Navbar.Brand>
                 </NavLink>
                 {currentUser && addPostIcon}
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle
+                     ref={ref} 
+                     onClick={() => setExpanded(!expanded)}
+                     aria-controls="basic-navbar-nav"
+                    />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-right">
 
