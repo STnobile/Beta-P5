75e1ba8d1481b92ccd8c7b34fc33f46682d07b12
STnobile <stnobile0@gmail.com>
2023-09-11 11:42:31 +0000

    added the utils file and the post files too, installed the scrolling lib.

--- src/App.js
+++ src/App.js
@@ -6,18 +6,53 @@ import './api/axiosDefaults';
 import SignUpForm from "./page/auth/SignUpForm";
 import SignInForm from "./page/auth/SignInForm";
 import PostCreateForm from "./page/posts/PostCreateForm";
+import PostPage from "./page/posts/PostPage";  
+import PostsPage from "./page/posts/PostsPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
+
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={()=> <h1>Home Page</h1> } />
+          <Route 
+          exact 
+          path="/" 
+          render={()=>( 
+          <PostsPage message="No results found. Adjust the search keyword." /> 
+          )} 
+          />
+
+          <Route 
+          exact 
+          path="/feed" 
+          render={()=>( 
+           <PostsPage 
+             message="No results found. Adjust the search keyword or follow a user." 
+             filter={`owner__followed__owner__profile=${profile_id}&`}
+           /> 
+          )} 
+          />
+
+          <Route 
+          exact 
+          path="/liked" 
+          render={()=>( 
+            <PostsPage 
+              message="No results found. Adjust the search keyword or like a post."
+              filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
+            /> 
+          )} 
+          />
           <Route exact path="/signin" render={()=> <SignInForm /> } />
           <Route exact path="/signup" render={()=> <SignUpForm /> } />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+          <Route exact path="/posts/:id" render={() => <PostPage />} />
           <Route render={()=> <p>Page Not Found!</p> } />
         </Switch>
       </Container>
